ac7feb6525a18cb58e77869b0d67c3b1b6099a18
simran maran <simranmaran10@gmail.com>
2025-05-20 14:53:02 +0530

    updated

---
+++ pattern.py
@@ -0,0 +1,95 @@
+# n=int(input("enter the number="))
+# i=1
+# while(i<=n):
+#     print(('*'*i))
+#     i=i+1
+
+
+# n=int(input("enter a number="))
+# i=1
+# while(i<=n):
+#    i=i+1
+    
+
+
+n=int(input("enter the number="))
+i=1
+while(i<=n):
+    print(' '*(n-i)+'*'*i)
+    i=i+1
+    
+
+
+# n=int(input("enter the number="))
+# i=0
+# while(i<n):
+#     print('*' *(n-i))
+#     i=i+1
+
+
+# n=int(input("enter the number="))
+# i=0
+# while(i<n):
+#     print(' '*i+'*'*(n-i))
+#     i=i+1
+
+
+# n=int(input("enter the number="))
+# i=0
+# while(i<n):
+#     print(' '*i+'*'*(n-i))
+#     i=i+1
+    
+
+# n=int(input("enter a number="))
+# i=1
+# while(i<n):
+#     print(' '*i+'*'*(n-i))
+#     i=i+1
+# i=i-2
+# while(i>=0):
+#     print(' '*i+'*'*(n-i))
+#     i=i-1
+
+
+
+# n=int(input("enter a number="))
+# i=1
+# while(i<n):
+#     print(' '*i+'* '*(n-i))
+#     i=i+1
+# i=i-2
+# while(i>=0):
+#     print(' '*i+'* '*(n-i))
+#     i=i-1
+
+# n=int(input("enter a number="))
+# i=0
+# while(i<n):
+#     print('*'*(n-i))
+#     i=i+1
+# i=i-2
+# while(i>=0):
+#     print('*'*(n-i))
+#     i=i-1
+
+
+# n=int(input("enter a number="))
+# i=0
+# while(i<n):
+#     print('* '*(n-i))
+#     i=i+1
+# i=i-2
+# while(i>=0):
+#     print('* '*(n-i))
+#     i=i-1
+
+
+# n=int(input("enter a number="))
+# i=1
+# while(i<n):
+#     print(''*(n-i)+'*'*i)
+#     i=i+1
+# i=i-2
+# while(i>0):
+#     print(''*(n-i)+'*'*i)
