815500c3eed8ec108a787bc324bb5b5045cf691b
simran maran <simranmaran10@gmail.com>
2025-05-24 23:23:41 +0530

    completed

---
+++ Q1.py
@@ -0,0 +1,41 @@
+#Q1
+# l=[10,20,30,40,50]
+# l[0],l[-1]=l[-1],l[0]
+# print(l)
+
+
+
+# Q2
+# t=(10,20,30,40,50)
+# l=list(t)
+# l[0],l[-1]=l[-1],l[0]
+# t=tuple(l)
+# print(t)
+
+#q3
+# s='python'
+# l=list(s)
+# l[0],l[-1]=l[-1],l[0]
+# print(l)
+# s=''.join(l)
+# print(s)
+
+
+# Q4
+# n=int(input("enter the numof th elements in the list"))
+# s=str(n)
+# l=list(s)
+# l[0],l[-1]=l[-1],l[0]
+# s=''.join(l)
+# print(s)
+
+
+n=int(input("enter the number="))
+for i in range(n):
+    x='A'
+    for j in range(0,n):
+        print(chr(ord(x)+j),end=' ')
+    print() 
+     
+    
+
