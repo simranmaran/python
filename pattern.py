72eb003b7a7a3d089b2a381d96cb405401521431
simran maran <simranmaran10@gmail.com>
2025-05-31 22:36:47 +0530

    done

--- pattern.py
+++ pattern.py
@@ -97,7 +97,13 @@
 
 
 
-for i in range(1,6):
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print("*",end=" ")
+#     print("\n")
+
+
+for i in range(5,0,-1):
     for j in range(1,i+1):
         print("*",end=" ")
     print("\n")
